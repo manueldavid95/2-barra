4f57f7ebfcad04adeda8510bb22f641d40e0b250
manueldavid95 <manueldavidrangel23@gmail.com>
2024-11-24 21:53:46 -0400

    finales

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const iconosMenu = document.getElementById('iconos-menu')
 const header = document.getElementById('header')
 const nav = document.getElementById('nav')
 const enlace = document.querySelectorAll('.list-link')
-
+const lista = document.querySelector('.list')
 
 
 
@@ -21,6 +21,13 @@ iconosMenu.addEventListener('click', palanca)
 // cerrar el menu
 enlace.forEach(element => {
     element.addEventListener('click', (e) => {
+        // lista.querySelector('.activo').classList.remove('activo')
+        // element.classList.add('activo')
+
+        // cambios al menu
+        document.querySelector('.activo').classList.remove('activo')
+        element.classList.add('activo')
+
         if (e.target.matches(`.${element.className}`)) {
             nav.classList.remove('active')
             header.classList.remove('active')
